fbcdaa2ae383823cf0721c22ccdd275e5d969342
leoniephelps <leoniephelps96@gmail.com>
2022-08-01 21:14:50 +0100

    add select topic by id

--- models/nc_news_model.js
+++ models/nc_news_model.js
@@ -5,3 +5,11 @@ exports.selectTopics = () => {
     return rows;
   });
 };
+
+exports.selectTopicById = (articleId) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id=$1;", [articleId])
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
