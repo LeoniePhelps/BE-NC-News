88fbb9eedec9c229ab4672ce22d3e21cebc2f5ef
leoniephelps <leoniephelps96@gmail.com>
2022-08-01 21:14:39 +0100

    add getarticlesbyid controller

--- controllers/nc_news_controller.js
+++ controllers/nc_news_controller.js
@@ -1,4 +1,4 @@
-const { selectTopics } = require("../models/nc_news_model");
+const { selectTopics, selectTopicById } = require("../models/nc_news_model");
 
 exports.getTopics = (req, res, next) => {
   selectTopics()
@@ -6,4 +6,11 @@ exports.getTopics = (req, res, next) => {
       res.status(200).send({ topics });
     })
     .catch(next);
+};
+
+exports.getArticlesById = (req, res, next) => {
+  const articleId = req.params.article_id;
+  selectTopicById(articleId).then((article) => {
+    res.status(200).send(article);
+  });
 };
