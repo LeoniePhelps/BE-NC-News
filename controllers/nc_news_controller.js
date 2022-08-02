2cab4ab5851df0d7f3eb966f72f53d416eb1139e
leoniephelps <leoniephelps96@gmail.com>
2022-08-02 10:55:48 +0100

    get articles by id controller

--- controllers/nc_news_controller.js
+++ controllers/nc_news_controller.js
@@ -10,7 +10,9 @@ exports.getTopics = (req, res, next) => {
 
 exports.getArticleById = (req, res, next) => {
   const articleId = req.params.article_id;
-  selectArticleById(articleId).then((article) => {
-    res.status(200).send(article);
-  });
+  selectArticleById(articleId)
+    .then((article) => {
+      res.status(200).send(article);
+    })
+    .catch(next);
 };
